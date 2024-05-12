339e9ada8a3eac4cef4136105fb1f71cd0804e64
Konrad <vobis08gw@gmail.com>
2024-05-12 21:03:45 +0200

    basic types

--- source/src/Typerio.tsx
+++ source/src/Typerio.tsx
@@ -1,4 +1,19 @@
+"use client";
 import React from "react";
+
+type TyperioInput = {
+  text: string;
+  style: string;
+  element: React.ElementType;
+};
+
+type TyperioConfig = {
+  //Todo
+};
+
+type TyperioProps = {
+  input: TyperioInput[];
+};
 
 const Typerio: React.FC = () => <span>Typerioowowo</span>;
 
