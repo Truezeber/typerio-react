16338d034a6be0d86ff29a08f3713802c72603b7
Konrad <vobis08gw@gmail.com>
2024-05-17 23:18:21 +0200

    editElement function

--- source/src/Typerio.tsx
+++ source/src/Typerio.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export type TyperioInput = {
   text: string;
@@ -16,13 +16,27 @@ export type TyperioProps = {
 };
 
 const Typerio: React.FC<TyperioProps> = ({ input }) => {
-  let elements: TyperioInput[] = [];
+  const [elements, setElements] = useState<TyperioInput[]>([]);
 
   const isOdd = (number: number) => number % 2 !== 0;
 
   const addElement = (inputObj: TyperioInput) => {
-    elements.push(inputObj);
+    const newElements = [...elements];
+    newElements.push(inputObj);
+    setElements(newElements);
   };
+
+  const editElement = (index: number, char: string) => {
+    const selectedElement = { ...elements[index] };
+    const newElements = [...elements];
+
+    selectedElement.text = `${selectedElement.text}${char}`;
+
+    newElements[index] = selectedElement;
+    setElements(newElements);
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <>
