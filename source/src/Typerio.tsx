a2df46ddcd52d3959a3720897b0cc9f7bdb7af5a
Konrad <vobis08gw@gmail.com>
2024-05-17 12:05:03 +0200

    rendering array redesigned

--- source/src/Typerio.tsx
+++ source/src/Typerio.tsx
@@ -1,17 +1,17 @@
 "use client";
 import React from "react";
 
-type TyperioInput = {
+export type TyperioInput = {
   text: string;
   style: string;
-  element: string;
+  element: keyof JSX.IntrinsicElements;
 };
 
-type TyperioConfig = {
+export type TyperioConfig = {
   //Todo
 };
 
-type TyperioProps = {
+export type TyperioProps = {
   input: TyperioInput[];
 };
 
@@ -19,16 +19,9 @@ const Typerio: React.FC<TyperioProps> = ({ input }) => {
   const isOdd = (number: number) => number % 2 !== 0;
   return (
     <>
-      {input.map((item, index) =>
-        React.createElement(
-          item.element,
-          {
-            key: index,
-            className: item.style,
-          },
-          item.text
-        )
-      )}
+      {input.map((item, index) => (
+        <item.element key={index}>{item.text}</item.element>
+      ))}
     </>
   );
 };
